9be87c5bdcc33bb72fb2d13e46134847c09bba47
en301 <emilsjostrand1000@gmail.com>
2023-11-22 10:11:16 +0100

    Add project files.

---
+++ uppgift 5.1 C/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uppgift_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mening = new string[3] { "Mening 1", "Mening 2 Wow" , "OMG DU KAN SKRIVA 3 meningar"};
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(mening[i]);
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
